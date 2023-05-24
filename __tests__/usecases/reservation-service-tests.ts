fd9d0da8fcbe9983ffa11f584f3fc30851153046
Mohsen-Bazmi <mohsen.bazmi@gmail.com>
2023-05-24 22:11:02 +0330

    test(reservation service): add test for app layer

---
+++ __tests__/usecases/reservation-service-tests.ts
@@ -0,0 +1,41 @@
+import { InMemoryReservationRepository } from "@/data-access/in-memory-reservations-repository";
+import { ReservedDayReadModel } from "@/domain/types";
+import { setReservationRepository } from "@/lib/dependencies";
+import { ReservationService, Reserve } from "@/usecases/reservation-service";
+
+describe('reservation service', () => {
+    const repository = InMemoryReservationRepository
+    beforeEach(() => setReservationRepository(repository));
+    afterEach(repository.clear);
+
+    it('reserves the car', async () => {
+        const today = new Date();
+
+        const command: Reserve = {
+            command: 'reserve',
+            reservedBy: 'Mohsen',
+            From: { date: today, at: 9 },
+            To: { date: today, at: 10 },
+        };
+
+        await ReservationService.handle(command);
+
+        expect(await repository.days()).toContain({
+            date: today,
+            numberOfHours: 1
+        });
+    });
+
+    //     it('cancels reservations', () => {
+
+    //         const command: CancelReservation = {
+    //             CommandType: 'cancelReservation',
+    //             ReservationId: ''
+    //         };
+
+    //         ReservationService.handle(command);
+
+    //         expect(true).toBeTruthy();
+
+    //     });
+})
